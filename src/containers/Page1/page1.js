bc037c803140920bb91df18b8137545736201abe
Alisher Maxkamov <alisher.maxkamov@unicorm.uz>
2023-05-17 15:32:42 +0500

    first_stage

---
+++ src/containers/Page1/page1.js
@@ -0,0 +1,13 @@
+import React, { Component} from 'react';
+import classes from './page1.module.css'
+class Page1 extends Component {
+    render() {
+         return(
+             <div className={classes.Page1}>
+                 <h1>Page1</h1>
+             </div>
+         )
+    }
+}
+
+export default Page1
